a63fe4181792b370c9b963aa8cf5f5b2226cb732
Pablo Godoy <pablodavid36@gmail.com>
2022-04-06 08:18:05 -0600

    refactor(qlik): se eleiminan elmentos innecesarios

--- ERP/qlik/models.py
+++ ERP/qlik/models.py
@@ -1,4 +1,5 @@
 import uuid
+
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext as _
--- ERP/qlik/views.py
+++ ERP/qlik/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormMixin
